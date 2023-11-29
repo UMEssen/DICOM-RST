fcaa5adcaefd185634dda5f88bbd8fe1e5c31b1e
Moritz von Göwels <moritz@tarn-vedra.de>
2023-11-29 13:43:57 +0100

    DicomConnection: Add timeout-handling, better tracing-information

--- src/pool/mod.rs
+++ src/pool/mod.rs
@@ -1,17 +1,14 @@
 use crate::config::{Aet, DicomConfig, PacsConfig};
 use crate::dimse::cecho::{CEchoRq, CEchoRsp};
-use crate::dimse::{
-    prepare_pdu_data, read_pdu_data, DicomError, FromDicomObject, IntoDicomObject, StatusType,
-};
+use crate::dimse::{prepare_pdu_data, read_pdu_data, FromDicomObject, IntoDicomObject, StatusType};
 use async_trait::async_trait;
 use deadpool::managed::{
     BuildError, Metrics, Object, Pool, QueueMode, RecycleError, RecycleResult,
 };
 use dicom::dictionary_std::uids::STUDY_ROOT_QUERY_RETRIEVE_INFORMATION_MODEL_FIND;
-use dicom::ul::ClientAssociationOptions;
 use std::collections::HashMap;
 use std::time::Duration;
-use tracing::{debug, info, warn};
+use tracing::{debug, info, info_span, warn, Instrument};
 
 mod connection;
 pub use self::connection::DicomConnection;
@@ -27,16 +24,19 @@ impl DicomPools {
         let mut pools = HashMap::with_capacity(dicom_config.pacs.len());
 
         for (aet, pacs_config) in &dicom_config.pacs {
+            let timeout = Duration::from_secs(pacs_config.pool_acquire_seconds);
+
             let mgr = DicomConnectionPool {
                 aet: aet.clone(),
                 pacs: pacs_config.clone(),
                 calling_ae_title: dicom_config.aet.clone(),
+                message_timeout: timeout,
             };
 
             let timeouts = deadpool::managed::Timeouts {
                 wait: None,
-                create: Some(Duration::from_secs(pacs_config.connect_timeout_seconds)),
-                recycle: Some(Duration::from_secs(pacs_config.connect_timeout_seconds)),
+                create: Some(timeout),
+                recycle: Some(timeout),
             };
 
             let pool = Pool::builder(mgr)
@@ -73,6 +73,7 @@ pub struct DicomConnectionPool {
     aet: Aet,
     pacs: PacsConfig,
     calling_ae_title: String,
+    message_timeout: Duration,
 }
 
 #[async_trait]
@@ -81,28 +82,27 @@ impl deadpool::managed::Manager for DicomConnectionPool {
     type Error = connection::Error;
 
     async fn create(&self) -> Result<Self::Type, Self::Error> {
-        info!(
-            "Establishing new client association for {} ({})",
-            self.aet, self.pacs.address
+        let backend_uuid = uuid::Uuid::new_v4();
+
+        let span = info_span!(
+            "backend_create",
+            backend_uuid = backend_uuid.to_string(),
+            aet = self.aet,
         );
 
-        // let options = ClientAssociationOptions::new()
-        //     .with_abstract_syntax(STUDY_ROOT_QUERY_RETRIEVE_INFORMATION_MODEL_FIND)
-        //     .calling_ae_title(self.calling_ae_title.clone())
-        //     .called_ae_title(self.aet.clone());
-
-        // let address = self.pacs.address.clone();
-
-        // tokio::task::spawn_blocking(move || Ok(options.establish_with(&address)?))
-        //     .await
-        //     .expect("tokio::task::spawn_blocking")
+        info!(
+            backend_uuid = backend_uuid.to_string(),
+            "Establishing new client association for {} ({})", self.aet, self.pacs.address
+        );
 
         DicomConnection::new(
+            backend_uuid,
             &self.pacs.address,
             &self.aet,
             &self.calling_ae_title,
             &STUDY_ROOT_QUERY_RETRIEVE_INFORMATION_MODEL_FIND,
         )
+        .instrument(span)
         .await
     }
 
@@ -111,77 +111,66 @@ impl deadpool::managed::Manager for DicomConnectionPool {
         client: &mut Self::Type,
         metrics: &Metrics,
     ) -> RecycleResult<Self::Error> {
-        info!(
-            "Recycling client association for {} ({}, age={:?} last_used={:?})",
-            self.aet,
-            self.pacs.address,
-            metrics.age(),
-            metrics.last_used()
+        let span = info_span!(
+            "backend_recycle",
+            backend_uuid = client.uuid().to_string(),
+            aet = self.aet,
         );
 
-        // if  rand::random::<bool>() {
-        //     warn!("Intentially returning Err from recycle()");
-        //     return Err(RecycleError::Message(format!("Random")));
-        // }
+        async {
+            info!(
+                "Recycling client association. age={:?} last_used={:?})",
+                metrics.age(),
+                metrics.last_used()
+            );
 
-        let c_echo_rq = CEchoRq::default()
-            .into_dicom_object()
-            .map_err(|err| RecycleError::Message(format!("Failed to create C-ECHO-RQ: {err}")))?;
+            // if  rand::random::<bool>() {
+            //     warn!("Intentially returning Err from recycle()");
+            //     return Err(RecycleError::Message(format!("Random")));
+            // }
 
-        let pctx = client
-            .presentation_contexts()
-            .first()
-            .ok_or(RecycleError::StaticMessage(
-                "Failed to get presentation context",
-            ))?;
+            let c_echo_rq = CEchoRq::default().into_dicom_object().map_err(|err| {
+                RecycleError::Message(format!("Failed to create C-ECHO-RQ: {err}"))
+            })?;
 
-        client
-            .send(prepare_pdu_data(&c_echo_rq, pctx.id))
-            .await
-            .map_err(|err| RecycleError::Message(format!("Failed to send C-ECHO-RQ: {err}")))?;
+            let pctx =
+                client
+                    .presentation_contexts()
+                    .first()
+                    .ok_or(RecycleError::StaticMessage(
+                        "Failed to get presentation context",
+                    ))?;
 
-        let response = client
-            .receive()
-            .await
-            .map_err(|err| RecycleError::Message(format!("Failed to receive C-ECHO-RSP: {err}")))?;
+            client
+                .send(prepare_pdu_data(&c_echo_rq, pctx.id), self.message_timeout)
+                .await
+                .map_err(|err| RecycleError::Message(format!("Failed to send C-ECHO-RQ: {err}")))?;
 
-        // let response = tokio::task::block_in_place(|| {
-        //     let pctx =
-        //         client
-        //             .presentation_contexts()
-        //             .first()
-        //             .ok_or(RecycleError::StaticMessage(
-        //                 "Failed to get presentation context",
-        //             ))?;
+            let response = client.receive(self.message_timeout).await.map_err(|err| {
+                RecycleError::Message(format!("Failed to receive C-ECHO-RSP: {err}"))
+            })?;
 
-        //     client
-        //         .send(&prepare_pdu_data(&c_echo_rq, pctx.id))
-        //         .map_err(|err| RecycleError::Message(format!("Failed to send C-ECHO-RQ: {err}")))?;
-        //     let response = client.receive().map_err(|err| {
-        //         RecycleError::Message(format!("Failed to receive C-ECHO-RSP: {err}"))
-        //     })?;
+            let response_object = read_pdu_data(&response).map_err(connection::Error::Dicom)?;
 
-        //     Result::<_, RecycleError<Self::Error>>::Ok(response)
-        // })?;
+            let c_echo_rsp =
+                CEchoRsp::from_dicom_object(&response_object).map_err(connection::Error::Dicom)?;
 
-        let response_object = read_pdu_data(&response).map_err(connection::Error::Dicom)?;
+            debug!("C-ECHO-RQ returned {:?}", c_echo_rsp.status_type);
 
-        let c_echo_rsp =
-            CEchoRsp::from_dicom_object(&response_object).map_err(connection::Error::Dicom)?;
-
-        debug!("C-ECHO-RQ returned {:?}", c_echo_rsp.status_type);
-
-        match c_echo_rsp.status_type {
-            StatusType::Success => Ok(()),
-            status => {
-                warn!(
-                    "Failed to recycle client association for {} ({}). C-ECHO-RQ returned {:?}",
-                    self.aet, self.pacs.address, status
-                );
-                Err(RecycleError::Message(
-                    format!("C-ECHO returned {status:?}",),
-                ))
+            match c_echo_rsp.status_type {
+                StatusType::Success => Ok(()),
+                status => {
+                    warn!(
+                        "Failed to recycle client association for {} ({}). C-ECHO-RQ returned {:?}",
+                        self.aet, self.pacs.address, status
+                    );
+                    Err(RecycleError::Message(
+                        format!("C-ECHO returned {status:?}",),
+                    ))
+                }
             }
         }
+        .instrument(span)
+        .await
     }
 }
