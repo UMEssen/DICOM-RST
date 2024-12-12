bdf571fcb743e83b4bdb4b2720580ce8a7aeb707
Nicolas Kamzol <nicolas.kamzol@uk-essen.de>
2024-12-12 09:28:58 +0100

    chore(format): fix internal rustfmt errors caused by whitespace in #[rustfmt::skip] blocks

--- src/backend/dimse/cecho/mod.rs
+++ src/backend/dimse/cecho/mod.rs
@@ -15,19 +15,19 @@ const COMMAND_FIELD_COMPOSITE_ECHO_RESPONSE: US = 0x8030;
 /// C-ECHO-RQ
 #[derive(Debug)]
 struct CompositeEchoRequest {
-    message_id: US,
+	message_id: US,
 }
 
 impl From<CompositeEchoRequest> for DicomMessage {
-    #[rustfmt::skip]
-    fn from(request: CompositeEchoRequest) -> Self {
+	#[rustfmt::skip]
+	fn from(request: CompositeEchoRequest) -> Self {
         let command = InMemDicomObject::command_from_element_iter([
             DataElement::new(tags::AFFECTED_SOP_CLASS_UID, VR::UI, dicom_value!(Str, uids::VERIFICATION)),
             DataElement::new(tags::COMMAND_FIELD, VR::US, dicom_value!(U16, [COMMAND_FIELD_COMPOSITE_ECHO_REQUEST])),
             DataElement::new(tags::MESSAGE_ID, VR::US, dicom_value!(U16, [request.message_id])),
             DataElement::new(tags::COMMAND_DATA_SET_TYPE, VR::US, dicom_value!(U16, [DATA_SET_MISSING]))
         ]);
-        
+
         Self {
             command,
             data: None,
@@ -39,20 +39,20 @@ impl From<CompositeEchoRequest> for DicomMessage {
 /// C-ECHO-RSP
 #[derive(Debug)]
 struct CompositeEchoResponse {
-    pub status: US,
+	pub status: US,
 }
 
 impl TryFrom<DicomMessage> for CompositeEchoResponse {
-    type Error = ReadError;
+	type Error = ReadError;
 
-    fn try_from(message: DicomMessage) -> Result<Self, Self::Error> {
-        let status = message
-            .command
-            .get(tags::STATUS)
-            .map(InMemElement::to_int::<US>)
-            .and_then(Result::ok)
-            .ok_or(Self::Error::MissingAttribute(tags::STATUS))?;
+	fn try_from(message: DicomMessage) -> Result<Self, Self::Error> {
+		let status = message
+			.command
+			.get(tags::STATUS)
+			.map(InMemElement::to_int::<US>)
+			.and_then(Result::ok)
+			.ok_or(Self::Error::MissingAttribute(tags::STATUS))?;
 
-        Ok(Self { status })
-    }
+		Ok(Self { status })
+	}
 }
